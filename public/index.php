ba39604ff3b44371a712280d416acf7e66ccba6f
Lucas Guimaraes <lucaslgr1206@gmail.com>
2020-07-13 13:14:35 -0300

    Sincronizando

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+    use Core\Core;
+
+    //Iniciando a $_SESSION
+    session_start();
+
+    //Liberando todos remetentes de requisições no CORS
+    header("Access-Control-Allow-Origin: *"); 
+    //Liberando todos os verbos HTTP
+    header("Access-Control-Allow-Methods: *");
+
+    //Importando os arquivos de setup
+    require 'config.php';
+    require 'routes.php';
+    require 'vendor/autoload.php';
+
+    //Iniciando a aplicacão
+    $c = new Core();
+    $c->run();
+?>
